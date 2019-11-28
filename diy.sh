bd755afc1ce183538ce004ef8ffc1a6c8d7761a9
kiss2u <joe.lamplight@gmail.com>
2019-11-28 02:11:09 +0000

    openwrt X86X64 11.28

--- diy.sh
+++ diy.sh
@@ -4,6 +4,9 @@
 #   Lisence: MIT
 #   Author: P3TERX
 #   Blog: https://p3terx.com
+#   Modified By Kiss2U
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.110/g' package/base-files/files/bin/config_generate
+# Modify OS Release
+sed -i 's/\'R9.11.23\'/\'R9.11.23\', Complied by Kiss2U/g' package/lean/default-settings/files/zzz-default-settings
