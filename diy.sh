bff4de1f11098021fc4d33aa787ef159c512b934
kiss2u <joe.lamplight@gmail.com>
2019-11-28 08:15:05 +0000

    opwrt lean x64 11.28

--- diy.sh
+++ diy.sh
@@ -7,6 +7,6 @@
 #   Modified By Kiss2U
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.110/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.100.110/g' package/base-files/files/bin/config_generate
 # Modify OS Release
-sed -i 's/\'R9.11.23\'/\'R9.11.23\', Complied by Kiss2U/g' package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/\'R9.11.23\'/\'R9.11.23\', Complied by Kiss2U/g' package/lean/default-settings/files/zzz-default-settings
