babc3045a12be03d66d71fe17464030e751b6e4b
duchenpaul <duchenpaul@gmail.com>
2019-11-29 21:50:36 +0800

    Add config

--- diy.sh
+++ diy.sh
@@ -7,3 +7,12 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+cd openwrt
+
+# wndr4300
+sed -i s/'23552k(ubi),25600k@0x6c0000(firmware)'/'120832k(ubi),122880k@0x6c0000(firmware)'/ target/linux/ar71xx/image/legacy.mk
+
+# Enable wifi
+sed -i 's/set wireless.radio${devidx}.disabled=1/set wireless.radio${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
