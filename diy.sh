0243ead6a2561080a83f5545016d5122ee32cef4
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2019-12-02 22:19:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/hostname='OpenWrt'/hostname='FreeJ233'/g' package/base-files/files/bin/config_generate
+sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+
