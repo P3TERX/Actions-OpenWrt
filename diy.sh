fe3fd809cff92c5e04aa1e86e7743b2ae70ba0bc
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2019-12-06 10:31:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,4 +9,4 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/'OpenWrt'/'FreeJ233'/g' package/base-files/files/bin/config_generate
 sed -i 's/root::0:0:99999:7:::/root:$1$xx6y.OEi$TtpqSyLy5Fz8SF8f9DRVy.:18217:0:99999:7:::/g' package/base-files/files/etc/shadow
-
+sed -i 's/src-git packages https://github.com/coolsnowwolf/packages/src-git lienol https://github.com/Lienol/openwrt-package/g' feeds.conf.default
