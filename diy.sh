3a4433d7390d10bef7e347acbd4c25c0651af6c7
FreeJ233 <50964071+FreeJ233@users.noreply.github.com>
2019-12-08 21:33:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,4 @@ sed -i 's/'OpenWrt'/'FreeJ233'/g' package/base-files/files/bin/config_generate
 sed -i 's/root::0:0:99999:7:::/root:$1$xx6y.OEi$TtpqSyLy5Fz8SF8f9DRVy.:18217:0:99999:7:::/g' package/base-files/files/etc/shadow
 git clone https://github.com/happyzhang1995/luci-app-adguardhome package/lean/luci-app-adguardhome
 git clone https://github.com/happyzhang1995/openwrt-adguardhome package/lean/openwrt-adguardhome
+git clone https://github.com/Lienol/openwrt-package package
