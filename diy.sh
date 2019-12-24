8e1b9d9727907c409346349b2856f35c5895a8d8
buddyfly <buddyfly@qq.com>
2019-12-24 21:48:00 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,5 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.99.2/192.168.99.255/g' package/base-files/files/bin/config_generate
 git clone https://github.com/Lienol/openwrt-package/tree/master/lienol/luci-app-passwall
