2d2a4af84b84923d4c9ffdd95801843433f262bb
xujianjie1213 <35050408+xujianjie1213@users.noreply.github.com>
2019-12-30 14:23:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
