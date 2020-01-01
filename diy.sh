d43f6af11281d7136c92bf73fa10851cad3a3f96
buddyfly <buddyfly@qq.com>
2020-01-01 21:35:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,5 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-# sed -i 's/192.168.99.2/192.168.99.255/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.99.1/192.168.99.255/g' package/base-files/files/bin/config_generate
 # git clone https://github.com/Lienol/openwrt-package
