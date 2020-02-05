1487e5a86261735785482e6e58a83aba4617e18d
jikuohong <34758287+jikuohong@users.noreply.github.com>
2020-02-05 14:26:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,16 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/hong0980/luci-app-passwall.git
+
+git clone https://github.com/jikuohong/luci-app-serverchan.git
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+
+git clone https://github.com/kuoruan/luci-app-kcptun.git package/luci-app-kcptun
+
+git clone https://github.com/jerrykuku/luci-app-vssr.git
+
+./scripts/feeds update -a
+
+./scripts/feeds install -a
