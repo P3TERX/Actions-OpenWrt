6925ca9fdd8ed1720fd47106ed6e070aca804620
liuleisail <30498867+liuleisail@users.noreply.github.com>
2020-03-27 18:35:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.2/192.168.50.5/g' package/base-files/files/bin/config_generate
+#git clone https://github.com/Lienol/openwrt
+#./scripts/feeds update -a
+#./scripts/feeds install -a
