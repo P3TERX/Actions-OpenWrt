8906655e9804052921c1c9f1acccc85bf1120808
hx1112 <50289984+hx1112@users.noreply.github.com>
2020-04-13 00:38:04 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,4 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
