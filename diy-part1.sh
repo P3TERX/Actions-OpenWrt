23d7fde2b7e748a875065b5e31344c65d925e247
P3TERX <p3terx@outlook.com>
2020-04-24 23:10:06 +0800

    Load custom feeds

---
+++ diy-part1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Lienol/openwrt-package
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
