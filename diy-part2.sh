48ce3482e63d7e337d13eefaa06224076bdb0e2b
jgsports <jgsports@gmail.com>
2020-06-01 08:02:55 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,4 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
