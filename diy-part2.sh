8bb7fe192bd6445d38b2205f71d9eaca6e29c303
mazheng127 <mazheng127@gmail.com>
2020-06-10 03:30:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,5 +8,11 @@
 # Blog: https://p3terx.com
 #============================================================
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+ Modify default IP
+sed -i 's/192.168.1.1/10.10.10.11/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tzxiaozhen88/luci-app-koolproxyR.git ./package/luci-app-koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luci-app-adguardhome
+# git clone https://github.com/pymumu/luci-app-smartdns.git ./package/luci-app-smartdns
+# git clone https://github.com/KFERMercer/luci-app-serverchan.git ./package/luci-app-serverchan
+git clone https://github.com/0saga0/openwrt-package.git ./package/luci-app-passwall
+git clone https://github.com/0saga0/openwrt-package.git ./package/luci-app-filebrowser
