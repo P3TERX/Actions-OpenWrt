5881dfc61ea75dfa0c652898548768966271e4c1
Atroc-0625 <37469514+Atroc-0625@users.noreply.github.com>
2020-06-24 18:50:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
 #sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 #sed -i 's/luci-theme-bootstrap/luci-theme-atmaterial/g' feeds/luci/collections/luci/Makefile
-sed -i '$\uci set system.@system[0].hostname=Atroc-x86' package/lean/default-settings/files
+sed -i '$/uci set system.@system[0].hostname=Atroc-x86' package/lean/default-settings/files
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
