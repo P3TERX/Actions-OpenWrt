029f1908d92cc4cde3c629310423dce07ab98d4e
Atroc-0625 <37469514+Atroc-0625@users.noreply.github.com>
2020-06-24 21:59:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
+# https://github.com/Atroc-0625/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author: Atroc-0625
 #============================================================
 
 # Modify default IP
