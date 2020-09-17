4d4afc9fa2bd5e5455a99cd3162a7bc5a0ec90f6
mine <15122691092@163.com>
2020-09-17 17:32:43 +0800

    Update diy-part0.sh

--- diy-part0.sh
+++ diy-part0.sh
@@ -13,17 +13,17 @@
 # 修改LAN IP
 #sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 device_name='Xiaomi_MIne'
-#echo "修改机器名称"
-#sed -i "s/OpenWrt/$device_name/g" ./package/base-files/files/bin/config_generate
+#echo '修改机器名称'
+#sed -i 's/OpenWrt/$device_name/g' ./package/base-files/files/bin/config_generate
 
 wifi_name=$device_name
-#echo "修改wifi名称"
-#sed -i "s/OpenWrt/$wifi_name/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#echo '修改wifi名称'
+#sed -i 's/OpenWrt/$wifi_name/g' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #samba4 开放root用户
-#sed -i "s/invalid/#invalid/g" ./package/lean/samba4/files/smb.conf.template
+#sed -i 's/invalid/#invalid/g' ./package/lean/samba4/files/smb.conf.template
 
-sed -i "s/openwrt.proxy.ustclug.org/mirrors.cloud.tencent.com/lede/g" ./package/lean/default-settings/files/zzz-default-settings
+sed -i 's/openwrt.proxy.ustclug.org/mirrors.cloud.tencent.com/lede/g' ./package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
