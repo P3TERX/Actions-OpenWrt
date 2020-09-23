b8574ea27c1eaf56203f1a5abb393ff3f222398d
mine <15122691092@163.com>
2020-09-23 16:17:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/garypang13/openwrt-packages' feeds.conf.default
 #!/bin/bash
 #filebrowser
 git clone https://github.com/filebrowser/filebrowser.git package/filebrowser
@@ -22,5 +22,4 @@ git clone https://github.com/filebrowser/filebrowser.git package/filebrowser
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #可道云
 git clone https://github.com/garypang13/luci-app-php-kodexplorer.git package/luci-app-php-kodexplorer
-#SmartDNS
-git clone https://github.com/garypang13/openwrt-packages package/
+
