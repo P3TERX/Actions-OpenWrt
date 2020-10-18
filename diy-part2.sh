77cf3aab2877fb98556aedc51efe2f8a3263023e
yusanxing <32813254+yusanxing@users.noreply.github.com>
2020-10-18 16:11:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,8 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 #============================================================
 git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
+git clone https://github.com/pymumu/luci-app-smartdns.git package/smartdns
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-#git clone https://github.com/Hyy2001X/luci-app-adguardhome.git package/adguardhome
 # git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
 #============================================================
 # 删除固件默认登录密码
