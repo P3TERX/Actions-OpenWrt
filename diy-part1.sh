aed850a963755ae6f14ba71e693f78e1a1dda170
brave-openwrt <73086706+brave-openwrt@users.noreply.github.com>
2020-10-19 10:04:42 +0800

    Update diy-part1.sh
    添加插件

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
