4d67e9bae7e8a0f553f07455e8fd056b25b1b3fb
yusanxing <32813254+yusanxing@users.noreply.github.com>
2020-10-20 16:09:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+  
+
+
