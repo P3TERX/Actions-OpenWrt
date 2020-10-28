d97d74ff2cecfe25bdd32372600dedf5b3fbea76
i5g5vip <58246606+i5g5vip@users.noreply.github.com>
2020-10-29 01:42:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 去除默认主题
+sed -i 's/+luci-theme-bootstrap/ /g' feeds/luci/collections/luci/Makefile
