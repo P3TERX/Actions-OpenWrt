7d5d3cc1592175b676eed9abd5b90035414f721e
i5g5vip <58246606+i5g5vip@users.noreply.github.com>
2020-10-29 01:43:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,5 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash;master' feeds.conf.default
