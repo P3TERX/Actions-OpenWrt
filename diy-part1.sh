6c11da4c22269fee4d77f0249eaf3c16796e0d35
chb100666 <41617754+chb100666@users.noreply.github.com>
2020-11-08 22:29:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small.git package/openwrt-small
