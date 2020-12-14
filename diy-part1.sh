b394b37786593dfd54be0b6bf9709028e73b0bad
i5g5vip <58246606+i5g5vip@users.noreply.github.com>
2020-12-15 00:36:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld' feeds.conf.default
