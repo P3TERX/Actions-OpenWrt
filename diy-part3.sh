498aafba215eb86da8e7c90c676dc9a7af9e2472
i5g5vip <58246606+i5g5vip@users.noreply.github.com>
2020-12-15 01:01:35 +0800

    Create diy-part3.sh

---
+++ diy-part3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/Mattraks/helloworld' feeds.conf.default
