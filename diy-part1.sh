4d81115e948b9fbf9d0028f2ea101d2bdb7aaa2d
yusanxing <32813254+yusanxing@users.noreply.github.com>
2021-01-18 22:31:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,14 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#============================================================
+git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+# git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/koolproxyR
+# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
+#============================================================
+# 删除固件默认登录密码
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+#=============================================================
