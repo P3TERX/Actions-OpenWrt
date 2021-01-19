c3b75327923ce45e8ef314242c79ad22ab778f39
yusanxing <32813254+yusanxing@users.noreply.github.com>
2021-01-19 20:50:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku-jd-dailybonus
 # git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/koolproxyR
 # git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
 #============================================================
