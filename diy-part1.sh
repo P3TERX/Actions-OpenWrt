7df106b8147d944716b0200e49e77f948f9c360e
yusanxing <32813254+yusanxing@users.noreply.github.com>
2021-01-19 17:57:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #============================================================
 git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
