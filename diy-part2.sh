4518099be1a7fc31e9d44310af851f193a4efb45
yusanxing <32813254+yusanxing@users.noreply.github.com>
2021-03-15 09:45:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.252/g' package/base-files/files/bin/config_generate
