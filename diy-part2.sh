8bb72cecce8b63365e9efc0ee895fbe0bf1be83a
pshang1973 <70361490+pshang1973@users.noreply.github.com>
2021-03-22 10:01:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.251/g' package/base-files/files/bin/config_generate
+git clone https://github.com/tuanqing/install-program package/install-program
