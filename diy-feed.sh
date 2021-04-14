e0a76acbafd91f6135f671e9bc6c88b126732032
lon91ong <lon91ong@gmail.com>
2021-04-14 14:47:34 +0800

    Update diy-feed.sh

--- diy-feed.sh
+++ diy-feed.sh
@@ -17,3 +17,4 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git garypang13 https://github.com/garypang13/openwrt-packages' feeds.conf.default
