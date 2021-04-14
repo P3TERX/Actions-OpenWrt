3bf252b2bb2bc3bcef9c6afa2a71bd65b8b2cb7f
superabbite1 <79433125+superabbite1@users.noreply.github.com>
2021-04-14 16:02:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,13 +15,12 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-mkdir liuchanghao
-cd liuchanghao
+cd package
 git clone https://github.com/xiaorouji/openwrt-passwall.git
-#git clone https://github.com/kenzok8/openwrt-packages.git
-#git clone https://github.com/kenzok8/small.git
-#git clone https://github.com/fw876/helloworld.git
-#git clone https://github.com/destan19/OpenAppFilter.git
-#git clone https://github.com/freifunk/openwrt-packages.git freifunk
-#./scripts/feeds update freifunk
-#./scripts/feeds install -a -p freifunk
+git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/small.git
+git clone https://github.com/fw876/helloworld.git
+git clone https://github.com/destan19/OpenAppFilter.git
+git clone https://github.com/freifunk/openwrt-packages.git freifunk/
+./scripts/feeds update freifunk
+./scripts/feeds install -a -p freifunk
