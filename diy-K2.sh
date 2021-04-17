87e4af61c02d514e012cf78b78feb12281060d47
lon91ong <lon91ong@gmail.com>
2021-04-17 20:03:08 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -27,6 +27,9 @@ sed -i 's/7872k/16064k/g' target/linux/ramips/image/mt7620.mk
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# 默认turboacc开启DNS Caching功能, 第二条汉化
+# 默认turboacc开启DNS Caching功能
 sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc
+
+# custom插件汉化
 mv package/feeds/custom/luci-app-turboacc/po/zh_Hans package/feeds/custom/luci-app-turboacc/po/zh-cn
+mv package/feeds/custom/luci-app-bypass/po/zh_Hans package/feeds/custom/luci-app-bypass/po/zh-cn
