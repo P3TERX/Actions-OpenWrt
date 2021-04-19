4a2233fb67279e25deb6f6485d67d2d9a2fa961a
lon91ong <lon91ong@gmail.com>
2021-04-19 19:11:27 +0800

    Update diy-Yk1.sh

--- Others/Youku-Yk1/diy-Yk1.sh
+++ Others/Youku-Yk1/diy-Yk1.sh
@@ -20,11 +20,11 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-set
 find target/linux -path "target/linux/*/config-*" | xargs -i sed -i '$a CONFIG_ACPI=y\nCONFIG_X86_ACPI_CPUFREQ=y\n \
 CONFIG_NR_CPUS=128\nCONFIG_FAT_DEFAULT_IOCHARSET="utf8"' {}
 
-# 修改默认主题
+# 修改默认主题, argon主题与lienol源适配有问题
 sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
 
-# 默认turboacc开启DNS Caching功能
-sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc
+# 默认turboacc开启DNS Caching功能, lienol似乎无需开启此功能
+# sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc
 
 # custom插件汉化
 mv package/feeds/custom/luci-app-turboacc/po/zh_Hans package/feeds/custom/luci-app-turboacc/po/zh-cn
