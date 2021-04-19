7627e579ce6c70b1fcec7b6795b502ef9846bf1e
lon91ong <lon91ong@gmail.com>
2021-04-19 22:49:01 +0800

    Update diy-feed.sh

--- diy-feed.sh
+++ diy-feed.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*telephony\)/\1/' feeds.conf.default
+sed -i 's/17.01/19.07/1' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
