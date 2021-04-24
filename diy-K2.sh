d51e7a30bb87f157b099e2b9017213834fd97564
lon91ong <lon91ong@gmail.com>
2021-04-24 15:36:10 +0800

    Update diy-K2.sh

--- diy-K2.sh
+++ diy-K2.sh
@@ -28,7 +28,9 @@ sed -i 's/DTS := PSG1218A/&\n\tIMAGE_SIZE := $(ralink_default_fw_size_16M)/g' ta
 #sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' feeds/luci/collections/luci/Makefile
 
 # 默认turboacc开启DNS Caching功能
-sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc
+#sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc
+# 修复DHCP服务, 从5.4内核改回4.14内核的resolv.conf路径
+sed -i 's|resolv.conf.d/resolv.conf.auto|resolv.conf.auto|g' `grep -l resolv.conf.d package/feeds/custom/*/root/etc/init.d/*`
 
 # turboacc插件汉化
 mv package/feeds/custom/luci-app-turboacc/po/zh_Hans package/feeds/custom/luci-app-turboacc/po/zh-cn
