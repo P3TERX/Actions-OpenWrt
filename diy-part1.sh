362f93a0e5d8de12febc58c4dd4001cc926daed1
yusanxing <32813254+yusanxing@users.noreply.github.com>
2021-04-29 16:24:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,15 +18,17 @@
 # git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 # git clone https://github.com/281677160/openwrt-package package/danshui
 #========================================================================================================
-# git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+git clone https://github.com/project-lede/luci-app-godproxy.git package/godproxy
+
 # git clone https://github.com/riverscn/openwrt-iptvhelper.git package/iptvhelper
-# git clone https://github.com/project-lede/luci-app-godproxy.git package/godproxy
 # git clone https://github.com/NateLol/luci-app-beardropper.git package/beardropper
 
 # git clone https://github.com/apple/cups.git package/cups
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku-jd-dailybonus
 # svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns package/luci-app-smartdns
 
+#https://github.com/jerrykuku/luci-theme-argon
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # git clone https://github.com/jefferymvp/luci-app-koolproxyR.git package/koolproxyR
