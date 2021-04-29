e10930a803424e2ab75b5592e01e99680fd8d3db
yusanxing <32813254+yusanxing@users.noreply.github.com>
2021-04-29 12:03:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,12 +18,12 @@
 # git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 # git clone https://github.com/281677160/openwrt-package package/danshui
 #========================================================================================================
-git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
-git clone https://github.com/riverscn/openwrt-iptvhelper.git package/iptvhelper
-git clone https://github.com/apple/cups.git package/cups
-git clone https://github.com/project-lede/luci-app-godproxy.git package/godproxy
-git clone https://github.com/NateLol/luci-app-beardropper.git package/beardropper
+# git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+# git clone https://github.com/riverscn/openwrt-iptvhelper.git package/iptvhelper
+# git clone https://github.com/project-lede/luci-app-godproxy.git package/godproxy
+# git clone https://github.com/NateLol/luci-app-beardropper.git package/beardropper
 
+# git clone https://github.com/apple/cups.git package/cups
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku-jd-dailybonus
 # svn co https://github.com/281677160/openwrt-package/trunk/luci-app-smartdns package/luci-app-smartdns
 
