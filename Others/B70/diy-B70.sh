83f1ffa5771022821186f447ebbc4a86c7153949
lon91ong <lon91ong@gmail.com>
2021-05-01 10:04:09 +0800

    Update diy-B70.sh

--- Others/B70/diy-B70.sh
+++ Others/B70/diy-B70.sh
@@ -20,11 +20,13 @@ sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/default-settings/files/zzz-default-set
 find target/linux -path "target/linux/*/config-*" | xargs -i sed -i '$a CONFIG_ACPI=y\nCONFIG_X86_ACPI_CPUFREQ=y\n \
 CONFIG_NR_CPUS=128\nCONFIG_FAT_DEFAULT_IOCHARSET="utf8"' {}
 
-#默认主题改为argon, lienol的主题设置别碰, 大坑
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+#默认主题改为argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+rm -rf feeds/custom/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/custom/luci-theme-argon
 
 # 网络接口适配
-sed -i ':a;N;$!ba;s/hc5962/&|\\\n\t&-spi/1' ./target/linux/ramips/mt7621/base-files/etc/board.d/02_network
+sed -i ':a;N;$!ba;s/hc5962/&|\\\n\t&-spi/g' ./target/linux/ramips/base-files/etc/board.d/02_network
 
 cat >> ./target/linux/ramips/image/mt7621.mk <<EOF
 define Device/hc5962-spi
@@ -39,12 +41,13 @@ EOF
 sed -i 's/^[ \t]*//g' ./target/linux/ramips/image/mt7621.mk
 
 #设置32m闪存
-#sed -i 's/0xfb0000/0x1fb0000/g' target/linux/ramips/dts/HC5962-SPI.dts
+sed -i 's/0xfb0000/0x1fa0000/g' target/linux/ramips/dts/HC5962-SPI.dts
 #sed -i 's/fw_size_16M/fw_size_32M/g' target/linux/ramips/image/mt7621.mk
 
 # 默认turboacc开启DNS Caching功能
 #sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc 
 # 修复DHCP服务, 从5.4内核改回4.14内核的resolv.conf路径
 sed -i 's|resolv.conf.d/resolv.conf.auto|resolv.conf.auto|g' `grep -l resolv.conf.d package/feeds/custom/*/root/etc/init.d/*`
+
 # custom插件汉化
 mv package/feeds/custom/luci-app-turboacc/po/zh_Hans package/feeds/custom/luci-app-turboacc/po/zh-cn
