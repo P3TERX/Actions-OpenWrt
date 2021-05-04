06e6a7e75ab5d2e83a05b9634c557fd5d11213c4
lon91ong <lon91ong@gmail.com>
2021-05-04 15:44:23 +0800

    Update diy-k2p.sh

--- Others/k2p/diy-k2p.sh
+++ Others/k2p/diy-k2p.sh
@@ -25,6 +25,10 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 rm -rf package/feeds/custom/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/custom/luci-theme-argon
 
+# 修复K2P无线丢失错误配置
+sed -i ':a;N;s/Phicomm K2P\nendef/Phicomm\sK2P\n\tDEVICE_PACKAGES := kmod-mt7603 kmod-mt76x2 wpad-basic\nendef/g;$!ba' target/linux/ramips/image/mt7621.mk
+sed -i 's/^[ \t]*//g' target/linux/ramips/image/mt7621.mk
+
 # 默认turboacc开启DNS Caching功能
 #sed -i -r "s/(dns_caching )'0'/\1'1'/1" package/feeds/custom/luci-app-turboacc/root/etc/config/turboacc
 # 修复DHCP服务, 从5.4内核改回4.14内核的resolv.conf路径
