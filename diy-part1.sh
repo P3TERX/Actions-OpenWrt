f75c4c27f241f9a883c3d9c8209c7a433c16bce3
Armin <visualarmin@gmail.com>
2021-05-04 13:12:42 +0430

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/xiaorouji/openwrt-passwall.git
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
