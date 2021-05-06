f1ee4ad6c042cea0edffa269b262e80e8db2ff59
Armin <visualarmin@gmail.com>
2021-05-07 02:24:51 +0430

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-cd pakages
+cd pakage
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/kenzok8/small.git
 #sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
