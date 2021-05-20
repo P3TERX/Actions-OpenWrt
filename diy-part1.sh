49b44a67062bf73ed25fdef8c006c0b141c2dcac
superabbite1 <79433125+superabbite1@users.noreply.github.com>
2021-05-20 12:36:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,7 @@ git clone https://github.com/liuran001/openwrt-packages.git
 #↓上面的包有问题的插件暂时删除#
 rm -rf openwrt-packages/adguardhome/
 rm -rf openwrt-packages/luci-app-vssr-plus/
+rm -rf openwrt-packages/luci-app-clash/
 #↓以下为liuran001/openwrt-packages中用到的依赖包#
 git clone https://github.com/wongsyrone/openwrt-Pcap_DNSProxy.git
 git clone https://github.com/superabbite1/atinout.git
@@ -32,25 +33,21 @@ git clone https://github.com/Leo-Jo-My/diy.git
 git clone https://github.com/Lienol/openwrt-package.git Lienol-package
 #↓此仓库包含了kenzok8/small和xiaorouji/openwrt-passwall，仅缺small里的openssl1.1，第一选择#
 git clone https://github.com/kenzok8/small-package.git
+#↓上面的包有问题的插件暂时删除#
+rm -rf small-package/luci-app-clash/
+rm -rf small-package/luci-app-openclash/
+rm -rf small-package/adguardhome/
 #git clone https://github.com/kenzok8/small.git
 #git clone https://github.com/xiaorouji/openwrt-passwall.git
-#↓上面的包有问题的插件暂时删除#
-rm -rf small-package/adguardhome/
-#↓另一种翻墙#
-git clone https://github.com/garypang13/luci-app-bypass.git
-git clone https://github.com/garypang13/smartdns-le.git
+
+#↓不使用kenzok8/small这个源时要单独使用这个clash #
+git clone https://github.com/frainzy1477/luci-app-clash.git
 #↓esir的关闭路由器#
 git clone https://github.com/esirplayground/luci-app-poweroff.git
 #↓定时设置插件（比自带的autoreboot功能多一些，二选一，也可同时选择编译成功，然后选择1个使用）#
 git clone https://github.com/sirpdboy/luci-app-autotimeset.git
 #↓一款流量统计插件#
 git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git
-#↓godproxy广告过滤#
-git clone https://github.com/project-lede/luci-app-godproxy.git
-#↓mentohust锐捷认证客户端 不确定是哪个包要测试#
-git clone https://github.com/jing955/Mentohust.git
-#git clone https://github.com/DragonBluep/mentohust-openwrt.git
-#git clone https://github.com/Apocalypsor/Actions-Openwrt-Custom.git
 #↓ddnsto内网穿透 不确定是哪个包要测试#
 git clone https://github.com/linkease/ddnsto-openwrt.git
 #git clone https://github.com/linkease/nas-packages.git
@@ -72,6 +69,15 @@ git clone https://github.com/rufengsuixing/luci-app-onliner.git
 #git clone https://github.com/Dreamacro/clash.git
 #https://github.com/awesome-openwrt/syncthing.git
 #https://github.com/awesome-openwrt/luci-app-syncthing.git
+#↓godproxy广告过滤 在kenzok8/small-package包含#
+#git clone https://github.com/project-lede/luci-app-godproxy.git
+#↓mentohust锐捷认证客户端 不确定是哪个包要测试  在kenzok8/small-package包含#
+#git clone https://github.com/jing955/Mentohust.git
+#git clone https://github.com/DragonBluep/mentohust-openwrt.git
+#git clone https://github.com/Apocalypsor/Actions-Openwrt-Custom.git
+#↓另一种翻墙 在kenzok8/small-package包含#
+#git clone https://github.com/garypang13/luci-app-bypass.git
+#git clone https://github.com/garypang13/smartdns-le.git
 
 #↓使用这个freifunk仓库需要以下内容#
 #git clone https://github.com/superabbite1/luci1.git
