c8b0db6a4b2151ff436354308aa2817093384108
qianmuyixiao <51079842+qianmuyixiao@users.noreply.github.com>
2021-05-28 05:16:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,30 +14,30 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #删除原来的argon
-#rm -rf  package/lean/luci-theme-argon
+rm -rf  package/lean/luci-theme-argon
 
 #添加老竭力的argon主题
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 #添加老竭力的argon.config
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
 #添加adguardhome插件
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 
 #添加OpenClash插件
-#git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
+git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 
 #添加passwall插件
-#git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/lean/openwrt-passwall
 
 #添加smartdns插件
-#git clone https://github.com/qianmuyixiao/luci-app-smartdns.git package/lean/luci-app-smartdns
-#git clone https://github.com/qianmuyixiao/smartdns.git package/lean/smartdns
+git clone https://github.com/qianmuyixiao/luci-app-smartdns.git package/lean/luci-app-smartdns
+git clone https://github.com/qianmuyixiao/smartdns.git package/lean/smartdns
 
 #添加vssr插件
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
 #添加一键安装到 emmc 脚本
 #git clone https://github.com/tuanqing/install-program.git package/lean/install-program
