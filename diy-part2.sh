5e93813cec4c91454d87a156ca43edec8ac8c3e2
qianmuyixiao <51079842+qianmuyixiao@users.noreply.github.com>
2021-05-28 19:35:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
 #添加adguardhome插件
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+git clone https://github.com/qianmuyixiao/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 git clone https://github.com/qianmuyixiao/adguardhome/tree/main.git package/lean/adguardhome
 
 #添加OpenClash插件
