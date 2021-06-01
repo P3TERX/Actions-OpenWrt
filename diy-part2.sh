403326013613a02edf9adaa33e175ce8c7ae882f
qianmuyixiao <51079842+qianmuyixiao@users.noreply.github.com>
2021-06-01 12:00:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,7 +20,7 @@ rm -rf  package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 #添加老竭力的argon.config
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-app-argon-config
 
 #添加adguardhome插件
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
